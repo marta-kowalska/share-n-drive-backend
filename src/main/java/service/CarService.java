f0781fc937e5ba82115e51f9af8bb83dfb93b774
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-20 18:04:54 +0200

    [add] CarService

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.Car;
+import model.CarType;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CarService {
+    public List<Car> getFeaturedCars() {
+        return null;
+    }
+
+    public List<Car> getAvailableCarsByDate(LocalDate from, LocalDate to) {
+        return null;
+    }
+
+
+    public List<Car> getAvailableCarsByType(List<CarType> types) {
+        return null;
+    }
+}
