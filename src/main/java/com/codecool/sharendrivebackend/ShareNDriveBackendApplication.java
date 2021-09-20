6b773d4b3d45cb127e811246aadf59b19ae8d20f
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-20 13:22:06 +0200

    [create] starter project

---
+++ src/main/java/com/codecool/sharendrivebackend/ShareNDriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.sharendrivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareNDriveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareNDriveBackendApplication.class, args);
+    }
+
+}
