4cc9cad3292fd528d8a229494f789ab333d13d48
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-20 19:35:42 +0200

    [refactor] car type enum

---
+++ src/main/java/com/codecool/sharendrivebackend/model/car/CarType.java
@@ -0,0 +1,9 @@
+package com.codecool.sharendrivebackend.model.car;
+
+public enum CarType {
+
+    HYBRID,
+    SELF_DRIVING,
+    ECO
+
+}
--- src/main/java/model/CarType.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public enum CarType {
-    ELECTRIC,
-    SELF_DRIVING
-}
