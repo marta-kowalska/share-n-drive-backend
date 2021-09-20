e97ab1ec8d0ad233e58c431ea82871cea5ce3ca9
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-20 19:41:05 +0200

    [add] basic car and customer build

--- src/main/java/com/codecool/sharendrivebackend/ShareNDriveBackendApplication.java
+++ src/main/java/com/codecool/sharendrivebackend/ShareNDriveBackendApplication.java
@@ -1,13 +1,50 @@
 package com.codecool.sharendrivebackend;
 
+import com.codecool.sharendrivebackend.dao.CarRepository;
+import com.codecool.sharendrivebackend.dao.CustomerRepository;
+import com.codecool.sharendrivebackend.model.car.BodyType;
+import com.codecool.sharendrivebackend.model.car.Car;
+import com.codecool.sharendrivebackend.model.car.FuelType;
+import com.codecool.sharendrivebackend.model.customer.Customer;
+import com.codecool.sharendrivebackend.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class ShareNDriveBackendApplication {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ShareNDriveBackendApplication.class, args);
     }
 
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init() {
+        return args -> {
+            Car bmw = Car.builder()
+                    .bodyType(BodyType.SEDAN)
+                    .brand("BMW")
+                    .fuelType(FuelType.ELECTRIC)
+                    .build();
+
+            Customer customer = Customer.builder()
+                    .firstName("Marta")
+                    .lastName("Kowalska")
+                    .car(bmw)
+                    .build();
+
+            bmw.setCustomer(customer);
+            customerRepository.save(customer);
+        };
+    }
 }
