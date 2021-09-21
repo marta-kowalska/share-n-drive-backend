5b7256b5398402d617ba626199bb4567aa6e46fd
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-22 01:29:18 +0200

    [add] findByPriceLessThanEqual and findBySeatNumberGreaterThanEqual

--- src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
+++ src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
@@ -3,5 +3,12 @@ package com.codecool.sharendrivebackend.dao;
 import com.codecool.sharendrivebackend.model.car.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface CarRepository extends JpaRepository<Car, Long>, CarRepositoryCustom {
+
+    List<Car> findByPriceLessThanEqual(int price);
+
+    List<Car> findBySeatNumberGreaterThanEqual(int seatNumber);
 }
