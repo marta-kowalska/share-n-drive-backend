c674b1568b5ce31f5b19591a3e9edb323c50f3b6
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-21 13:31:52 +0200

    [add] CarRepositoryCustom extension to CarRepository

--- src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
+++ src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
@@ -3,5 +3,5 @@ package com.codecool.sharendrivebackend.dao;
 import com.codecool.sharendrivebackend.model.car.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarRepository extends JpaRepository<Car, Long> {
+public interface CarRepository extends JpaRepository<Car, Long>, CarRepositoryCustom {
 }
