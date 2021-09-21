d417608efc816f470a4de76d0f4b955194edec10
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-22 01:10:18 +0200

    [add] name field and getTypeByName method

--- src/main/java/com/codecool/sharendrivebackend/model/car/FuelType.java
+++ src/main/java/com/codecool/sharendrivebackend/model/car/FuelType.java
@@ -2,9 +2,23 @@ package com.codecool.sharendrivebackend.model.car;
 
 public enum FuelType {
 
-    GAS,
-    GASOLINE,
-    DIESEL,
-    ELECTRIC,
+    GAS("gas"),
+    GASOLINE("gasoline"),
+    DIESEL("diesel"),
+    ELECTRIC("electric");
 
+    private final String name;
+
+    FuelType(String name) {
+        this.name = name;
+    }
+
+    public static FuelType getTypeByName(String searchedName){
+        for(FuelType type : values()){
+            if(type.name.equals(searchedName)){
+                return type;
+            }
+        }
+        return null;
+    }
 }
