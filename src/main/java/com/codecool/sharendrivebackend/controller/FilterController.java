8af1cde8db81c9b71d7194521d7308a3c8ee498c
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-21 09:51:05 +0200

    [add] FilterController with basic routes

---
+++ src/main/java/com/codecool/sharendrivebackend/controller/FilterController.java
@@ -0,0 +1,33 @@
+package com.codecool.sharendrivebackend.controller;
+
+import com.codecool.sharendrivebackend.model.car.Car;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.codecool.sharendrivebackend.service.CarService;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/share-n-drive")
+public class FilterController {
+
+    private final CarService carService;
+
+    @Autowired
+    public FilterController(CarService carService){
+        this.carService = carService;
+    }
+
+    @GetMapping("/featured")
+    public List<Car> getFeaturedCars() {
+        return carService.getFeaturedCars();
+    }
+
+    @RequestMapping(value="/filter", method = RequestMethod.GET)
+    public List<Car> getParams(@RequestParam Map<String, String> params ) {
+        return carService.getAvailableCarsByFilter(params);
+    }
+
+
+}
