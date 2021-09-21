be98ffaa579c287e618c638f414ca0812275e2a8
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-21 13:22:52 +0200

    [add] save one car into the database

--- src/test/java/com/codecool/sharendrivebackend/dao/CarRepositoryTest.java
+++ src/test/java/com/codecool/sharendrivebackend/dao/CarRepositoryTest.java
@@ -1,7 +1,38 @@
 package com.codecool.sharendrivebackend.dao;
 
+import com.codecool.sharendrivebackend.model.car.Car;
+import com.codecool.sharendrivebackend.model.car.FuelType;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ActiveProfiles("test")
 class CarRepositoryTest {
 
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void saveOneCar() {
+        Car volkswagen = Car.builder()
+                .seatNumber(5)
+                .fuelType(FuelType.ELECTRIC)
+                .brand("Volkswagen")
+                .build();
+
+        carRepository.save(volkswagen);
+        assertEquals(1, carRepository.findAll().size());
+    }
 }
