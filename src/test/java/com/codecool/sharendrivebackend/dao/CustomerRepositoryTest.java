b42b2f12ad80954976074d75ce97de079d5e9b63
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-22 15:21:30 +0200

    [add] save customer into database tests with and without address or car

--- src/test/java/com/codecool/sharendrivebackend/dao/CustomerRepositoryTest.java
+++ src/test/java/com/codecool/sharendrivebackend/dao/CustomerRepositoryTest.java
@@ -1,7 +1,66 @@
 package com.codecool.sharendrivebackend.dao;
 
+import com.codecool.sharendrivebackend.model.address.Address;
+import com.codecool.sharendrivebackend.model.car.Car;
+import com.codecool.sharendrivebackend.model.customer.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")
 class CustomerRepositoryTest {
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void saveCustomerWithoutCarOrAddress() {
+        Customer peter = Customer.builder()
+                .firstName("Peter")
+                .lastName("Graham")
+                .build();
+
+        customerRepository.save(peter);
+
+        assertEquals(1, customerRepository.findAll().size());
+
+        List<Customer> customers = customerRepository.findAll();
+        assertTrue(customers.stream().anyMatch(customer -> customer.getAddress() == null));
+        assertTrue(customers.stream().anyMatch(customer -> customer.getCars().isEmpty()));
+    }
+
+    @Test
+    public void saveCustomerWithAddressOrWithCars() {
+        Customer peter = Customer.builder()
+                .firstName("Peter")
+                .lastName("Graham")
+                .address(Address.builder().address("address st. 1").build())
+                .build();
+
+        Customer jacob = Customer.builder()
+                .firstName("Jacob")
+                .lastName("Graham")
+                .car(Car.builder().build())
+                .build();
+
+        customerRepository.save(peter);
+        customerRepository.save(jacob);
+
+        assertTrue(customerRepository.findAll().stream().anyMatch(customer -> customer.getAddress() == null));
+        assertTrue(customerRepository.findAll().stream().anyMatch(customer -> customer.getCars().isEmpty()));
+    }
 }
