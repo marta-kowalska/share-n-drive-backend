660d62826c999bd23f265b175119acba90828c44
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-23 12:02:51 +0200

    [add] car must have one customer and after save a customer with car then it also saved into DB

--- src/test/java/com/codecool/sharendrivebackend/dao/CarRepositoryTest.java
+++ src/test/java/com/codecool/sharendrivebackend/dao/CarRepositoryTest.java
@@ -2,26 +2,31 @@ package com.codecool.sharendrivebackend.dao;
 
 import com.codecool.sharendrivebackend.model.car.Car;
 import com.codecool.sharendrivebackend.model.car.FuelType;
+import com.codecool.sharendrivebackend.model.customer.Customer;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 @ActiveProfiles("test")
 class CarRepositoryTest {
 
     @Autowired
     private CarRepository carRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Autowired
     private EntityManager entityManager;
@@ -57,5 +62,33 @@ class CarRepositoryTest {
                 .build();
 
         carRepository.saveAndFlush(car2);
+    }
+
+    @Test
+    public void afterSaveCustomerWithCars_ThenCarsShouldBeInDatabase() {
+        Car car1 = Car.builder().build();
+        Car car2 = Car.builder().build();
+        Car car3 = Car.builder().build();
+
+        Customer customer = Customer.builder()
+                .car(car1)
+                .car(car2)
+                .car(car3)
+                .build();
+
+        carRepository.saveAll(Arrays.asList(car1, car2, car3));
+        List<Car> cars = carRepository.findAll();
+        assertTrue(cars.size() > 2);
+    }
+
+    @Test
+    public void oneCarMustHaveAndOnlyOneOwner() {
+        Car bmw = Car.builder().brand("BMW").build();
+        Customer customer = Customer.builder().car(bmw).build();
+
+        customerRepository.save(customer);
+
+        List<Customer> customers = customerRepository.findAllCustomerByGivenCarName("BMW");
+        assertEquals(customers.size(), carRepository.findAll().stream().filter(car -> car.getCustomer() == null).count());
     }
 }
