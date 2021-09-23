b6207eea09380a01ca3901687d75f3b67d8ef706
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-09-23 12:01:09 +0200

    [add] query for search customers by car name

--- src/main/java/com/codecool/sharendrivebackend/dao/CustomerRepository.java
+++ src/main/java/com/codecool/sharendrivebackend/dao/CustomerRepository.java
@@ -2,6 +2,13 @@ package com.codecool.sharendrivebackend.dao;
 
 import com.codecool.sharendrivebackend.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Query("select c.customer from Car c")
+    List<Customer> findAllCustomerByGivenCarName(@Param("name") String carName);
 }
