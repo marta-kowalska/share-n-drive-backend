a9e13a283001e21b136a0f0050cf6600ad8b56ec
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-23 11:14:59 +0200

    [add] findById method

--- src/main/java/com/codecool/sharendrivebackend/dao/CustomerRepository.java
+++ src/main/java/com/codecool/sharendrivebackend/dao/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.codecool.sharendrivebackend.dao;
 import com.codecool.sharendrivebackend.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findById(Long id);
 }
