99370e4da73b375b1a7346b31b16f324b8c1d010
NorbertRuff <ruffnorbert88@gmail.com>
2021-09-27 18:28:50 +0200

    [add] transmission types enum

---
+++ src/main/java/com/codecool/sharendrivebackend/model/car/Transmission.java
@@ -0,0 +1,22 @@
+package com.codecool.sharendrivebackend.model.car;
+
+public enum Transmission {
+    MANUAL("MANUAL"),
+    AUTOMATIC("AUTOMATIC");
+
+    private final String name;
+
+    Transmission(String name) {
+        this.name = name;
+    }
+
+    public static Transmission getTypeByName(String searchedName) {
+        for (Transmission type : values()) {
+            if (type.name.equals(searchedName)) {
+                return type;
+            }
+        }
+        return null;
+    }
+}
+
