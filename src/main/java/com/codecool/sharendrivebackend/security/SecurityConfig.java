8d75b24e6aa647f2c4fefa908a491dd8cbb200eb
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-10-05 13:52:29 +0200

    [change] /customer/{customerId} to /customer-details

--- src/main/java/com/codecool/sharendrivebackend/security/SecurityConfig.java
+++ src/main/java/com/codecool/sharendrivebackend/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.POST, "/auth/signin").permitAll()
             .antMatchers(HttpMethod.POST, "/share-n-drive/add-car").authenticated()
             .antMatchers(HttpMethod.GET, "/share-n-drive/bookings").authenticated()
-            .antMatchers(HttpMethod.GET, "/share-n-drive/customer/*").authenticated()
+            .antMatchers(HttpMethod.GET, "/share-n-drive/customer-details").authenticated()
             .antMatchers(HttpMethod.POST, "/share-n-drive/book-car").authenticated()
             .antMatchers(HttpMethod.DELETE, "/share-n-drive/remove-car/*").authenticated()
             .and()
