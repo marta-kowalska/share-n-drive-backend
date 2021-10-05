e386f412e9429ebe2b31fe486502c6d00698cac1
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-10-05 13:21:41 +0200

    [add] removeCarByIdWhereUserIdCorrect

--- src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
+++ src/main/java/com/codecool/sharendrivebackend/dao/CarRepository.java
@@ -1,8 +1,12 @@
 package com.codecool.sharendrivebackend.dao;
 
 import com.codecool.sharendrivebackend.model.car.Car;
+import com.codecool.sharendrivebackend.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,4 +26,11 @@ public interface CarRepository extends JpaRepository<Car, Long>, CarRepositoryCu
 
     @Query("select DISTINCT c.brand from Car c")
     List<String> getBrands();
+
+    @Transactional
+    @Modifying
+    @Query("delete from Car c where c.id = :carId and c.customer = :userId")
+    void removeCarByIdWhereUserIdCorrect(@Param("carId") Long carId, @Param("userId") Customer userId);
+
+
 }
